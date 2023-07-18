85cd84a5e09b2d6f3faf638c7cd127d14d2c10ac
gabi <70315572+Limgabi@users.noreply.github.com>
2023-07-18 13:39:38 +0900

    style : global style 적용

--- src/presentation/components/App.tsx
+++ src/presentation/components/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "react-redux";
 import store from "../store/store";
+import { GlobalStyle } from "../styles/GlobalStyle";
 
 function App() {
   const queryClient = new QueryClient();
   return (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
+        <GlobalStyle />
         <></>
       </QueryClientProvider>
     </Provider>
