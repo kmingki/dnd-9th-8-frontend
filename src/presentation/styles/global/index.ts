35d7c4b51bb799526fdc69cd55b6ced85cd1f43e
kmingki <wl1524@naver.com>
2023-07-31 20:14:24 +0900

    style #1 : global style max width 변경

--- src/presentation/styles/global/index.ts
+++ src/presentation/styles/global/index.ts
@@ -1,8 +1,17 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+import { MAX_WIDTH } from '@constants';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
+  html,
+  body,
+  #root {
+    background-color: rgba(0, 0, 0, 0.2);
+    max-width: ${MAX_WIDTH};
+    height: 100%;
+    margin: 0 auto;
+  }
 `;
 
 export default GlobalStyle;
