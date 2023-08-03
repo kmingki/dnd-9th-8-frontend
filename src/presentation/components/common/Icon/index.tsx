6b2bd0f44b035bfbffa2c26593765cf72463281c
gabi <70315572+Limgabi@users.noreply.github.com>
2023-08-03 21:57:35 +0900

    style #5 : 디자인 시스템 Icon

---
+++ src/presentation/components/common/Icon/index.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+import * as icons from "../Icon/icons";
+
+interface IconProps {
+  icon: string;
+  width?: number;
+  height?: number;
+  rotate?: number;
+
+  fill?: string;
+  color?: string;
+}
+
+const Icon = ({ icon, width, height, rotate, fill, color }: IconProps) => {
+  const IconComponent = icons[icon as keyof typeof icons];
+  return (
+    <IconWrapper
+      width={width}
+      height={height}
+      rotate={rotate}
+      fill={fill}
+      color={color}
+    >
+      <IconComponent />
+    </IconWrapper>
+  );
+};
+
+const IconWrapper = styled.div<{
+  width?: number;
+  height?: number;
+  rotate?: number;
+  fill?: string;
+  color?: string;
+}>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: ${({ width }) => width}px;
+  height: ${({ height }) => height}px;
+
+  ${({ rotate }) =>
+    rotate && {
+      transform: `rotate(${rotate}deg)`,
+    }}
+  svg {
+    width: ${({ width }) => width}px;
+    height: ${({ height }) => height}px;
+
+    path {
+      stroke: ${({ color }) => color};
+      fill: ${({ fill }) => fill};
+    }
+  }
+`;
+
+export default Icon;
