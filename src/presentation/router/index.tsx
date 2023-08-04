47a7c7732a56b91331d652d270adf0d83a5a6bd4
gabi <70315572+Limgabi@users.noreply.github.com>
2023-08-04 20:59:39 +0900

    feat #11 : 로그인 페이지 라우팅

--- src/presentation/router/index.tsx
+++ src/presentation/router/index.tsx
@@ -1,9 +1,12 @@
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import LoginPage from "../pages/LoginPage";
+
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<div>Test</div>} />
+        <Route path="login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
