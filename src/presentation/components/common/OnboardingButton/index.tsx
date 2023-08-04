68ca8f03a7b05fc4064894461f57f7b5b6ed9bc1
gabi <70315572+Limgabi@users.noreply.github.com>
2023-08-05 01:16:56 +0900

    fix #4 : 디자인 시스템 속성 추가

--- src/presentation/components/common/OnboardingButton/index.tsx
+++ src/presentation/components/common/OnboardingButton/index.tsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { styled, css } from "styled-components";
 import COLOR from "@styles/colors";
@@ -6,53 +5,57 @@ import Icon from "../Icon";
 import { DESTINATION } from "@constants";
 
 type OnboardingButtonType = {
-    children? : JSX.Element;
-    dest?: string;
-    isChecked?: boolean;
-    text?: string;
-  };
-  
+  children?: JSX.Element;
+  dest?: string;
+  isChecked?: boolean;
+  text?: string;
+  onClick: any;
+};
 
-const OnboardingButton = ({ children , dest, isChecked, text }: OnboardingButtonType) => {
-    return (
-        <OnboardingButtonWapper isChecked={isChecked}>
-        
-        { isChecked && <IconWrapper><Icon icon="Check" /></IconWrapper>}
+const OnboardingButton = ({
+  children,
+  dest,
+  isChecked,
+  text,
+  onClick,
+}: OnboardingButtonType) => {
+  return (
+    <OnboardingButtonWapper isChecked={isChecked} onClick={onClick}>
+      {isChecked && (
+        <IconWrapper>
+          <Icon icon="Check" />
+        </IconWrapper>
+      )}
 
-        {dest === DESTINATION.abroad ? <Icon icon="Plane"  /> :
-        <Icon icon="Bus"  />
-        }
-        <Text>
-          {text}
-        </Text>
-        </OnboardingButtonWapper>
-    );
-}
+      {dest === DESTINATION.abroad ? <Icon icon="Plane" /> : <Icon icon="Bus" />}
+      <Text>{text}</Text>
+    </OnboardingButtonWapper>
+  );
+};
 
 const OnboardingButtonWapper = styled.button<{ isChecked?: boolean }>`
   width: 100%;
   height: 80px;
   border-radius: 10px;
-  border : none ;
+  border: none;
   ${({ isChecked }) =>
-  isChecked && 
+    isChecked &&
     css`
-    border: 1px solid ${COLOR.MAIN_GREEN};
+      border: 1px solid ${COLOR.MAIN_GREEN};
     `}
   background-color: ${COLOR.GRAY_50};
   outline: none;
-  position : relative;
+  position: relative;
 `;
-
 
 const Text = styled.p`
-color: ${COLOR.GRAY_800};
+  color: ${COLOR.GRAY_800};
 `;
 
-const IconWrapper = styled.p`
-  position : absolute;
-  top : 9px;
-  right : 9px;
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 9px;
+  right: 9px;
 `;
 
 export default OnboardingButton;
