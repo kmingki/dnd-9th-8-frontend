b957b6c2294fb9a261a7703eb6dccc1b3eefa8b1
gabi <70315572+Limgabi@users.noreply.github.com>
2023-08-05 13:48:21 +0900

    feat #11 : 로그인 완료 페이지 라우팅

--- src/presentation/router/index.tsx
+++ src/presentation/router/index.tsx
@@ -1,13 +1,15 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import TripCreatePage from "../pages/TripCreatePage";
+import LoginCompletePage from "../pages/LoginComplatePage";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<div></div>} />
-        <Route path="login" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/login/complete" element={<LoginCompletePage />} />
         <Route path="/trip-create/:step" element={<TripCreatePage />} />
       </Routes>
     </BrowserRouter>
