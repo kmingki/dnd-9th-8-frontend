4c5b3245497c61ffa07688a8181bac7a6ce66cda
gabi <70315572+Limgabi@users.noreply.github.com>
2023-08-06 15:46:07 +0900

    style #23 : 메인 홈 관련 아이콘 추가

--- src/presentation/components/common/Icon/icons/index.tsx
+++ src/presentation/components/common/Icon/icons/index.tsx
@@ -10,3 +10,9 @@ export { default as Check } from "./Check";
 export { default as InputChecked } from "./InputChecked";
 export { default as Checked } from "./Checked";
 export { default as Plus } from "./Plus";
+
+export { default as Logo } from "./Logo";
+export { default as Folder } from "./Folder";
+export { default as User } from "./User";
+
+export { default as NotExistCheckList } from "./NotExistCheckList";
