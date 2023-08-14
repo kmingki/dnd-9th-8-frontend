e1bb1a91385613cd6401c15dbda5ae9cf05f2780
gabi <70315572+Limgabi@users.noreply.github.com>
2023-08-15 01:21:50 +0900

    fix #44 : 로그인 완료 화면 디자인 수정사항 반영

--- src/presentation/pages/LoginComplatePage/index.tsx
+++ src/presentation/pages/LoginComplatePage/index.tsx
@@ -21,22 +21,20 @@ const LoginCompletePage = () => {
     <LoginCompleteWrapper>
       <TextWrapper>
         <Text
-          color={COLOR.GRAY_900}
+          color={COLOR.MAIN_GREEN}
           fontSize={30}
-          fontWeight={700}
+          fontWeight={600}
           lineHeight="140%"
           text="환영합니다"
         />
         <div className="sub-text">
-          패킷의 체크리스트로
+          패-킷과 체크리스트로
           <br />
-          마음 편한 여행 준비를 시작하세요
+          여행 준비를 시작하세요
         </div>
       </TextWrapper>
       <Spacing size={32} />
-      <IconWrapper>
-        <Icon icon="LoginComplete" />
-      </IconWrapper>
+      <Icon icon="LoginComplete" />
       <BottomButton text="패킷 시작하기" onClick={handleClickStart} />
     </LoginCompleteWrapper>
   );
@@ -48,21 +46,17 @@ const LoginCompleteWrapper = styled.div`
 const TextWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 11px;
+  gap: 7px;
 
   margin-top: 40%;
 
   text-align: center;
 
   .sub-text {
-    color: ${COLOR.GRAY_600};
-    font-size: 18px;
-    font-weight: 500;
+    color: ${COLOR.GRAY_700};
+    font-size: 20px;
+    font-weight: 600;
     line-height: 132%;
   }
-`;
-const IconWrapper = styled.div`
-  display: flex;
-  justify-content: center;
 `;
 export default LoginCompletePage;
