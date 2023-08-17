37e8a7caf44f367de626aa28485dd35e288522ab
gabi <70315572+Limgabi@users.noreply.github.com>
2023-08-17 19:14:12 +0900

    fix #50 : 아이콘 svg 수정

--- src/presentation/components/common/Icon/icons/KakaoLogo.tsx
+++ src/presentation/components/common/Icon/icons/KakaoLogo.tsx
@@ -2,17 +2,42 @@ import React from "react";
 
 const KakaoLogo = () => {
   return (
-    <svg width="65" height="65" viewBox="0 0 65 65" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect x="0.802619" y="0.529182" width="63.0588" height="63.0588" rx="14.5882" fill="#FEE500" stroke="#E5CF0A" stroke-width="0.941176"/>
-<g clip-path="url(#clip0_1100_21842)">
-<path fill-rule="evenodd" clip-rule="evenodd" d="M32.3328 19.5793C25.0552 19.5793 19.1563 24.1609 19.1563 29.8116C19.1563 33.3258 21.4379 36.4238 24.9122 38.2665L23.4504 43.6349C23.3212 44.1092 23.8609 44.4873 24.2753 44.2124L30.6833 39.9609C31.224 40.0133 31.7736 40.0439 32.3328 40.0439C39.6098 40.0439 45.5091 35.4625 45.5091 29.8116C45.5091 24.1609 39.6098 19.5793 32.3328 19.5793Z" fill="black"/>
-</g>
-<defs>
-<clipPath id="clip0_1100_21842">
-<rect width="26.3529" height="26.3529" fill="white" transform="translate(19.1562 18.8823)"/>
-</clipPath>
-</defs>
-</svg>
+    <svg
+      width="65"
+      height="65"
+      viewBox="0 0 65 65"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="0.802619"
+        y="0.529182"
+        width="63.0588"
+        height="63.0588"
+        rx="14.5882"
+        fill="#FEE500"
+        stroke="#E5CF0A"
+        strokeWidth="0.941176"
+      />
+      <g clip-path="url(#clip0_1100_21842)">
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M32.3328 19.5793C25.0552 19.5793 19.1563 24.1609 19.1563 29.8116C19.1563 33.3258 21.4379 36.4238 24.9122 38.2665L23.4504 43.6349C23.3212 44.1092 23.8609 44.4873 24.2753 44.2124L30.6833 39.9609C31.224 40.0133 31.7736 40.0439 32.3328 40.0439C39.6098 40.0439 45.5091 35.4625 45.5091 29.8116C45.5091 24.1609 39.6098 19.5793 32.3328 19.5793Z"
+          fill="black"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_1100_21842">
+          <rect
+            width="26.3529"
+            height="26.3529"
+            fill="white"
+            transform="translate(19.1562 18.8823)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
   );
 };
 
