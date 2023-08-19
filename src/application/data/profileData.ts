02c44408ab6b33c19c7dc365f638020f7fc113c1
gabi <70315572+Limgabi@users.noreply.github.com>
2023-08-19 19:53:24 +0900

    design : 프로필 디자인 수정사항 반영

---
+++ src/application/data/profileData.ts
@@ -0,0 +1,6 @@
+export const profileData = [
+  "https://dnd--pack-it.s3.ap-northeast-2.amazonaws.com/profile-images/1.svg",
+  "https://dnd--pack-it.s3.ap-northeast-2.amazonaws.com/profile-images/2.svg",
+  "https://dnd--pack-it.s3.ap-northeast-2.amazonaws.com/profile-images/3.svg",
+  "https://dnd--pack-it.s3.ap-northeast-2.amazonaws.com/profile-images/4.svg",
+];
--- src/presentation/pages/EditMyInfoPage/index.tsx
+++ src/presentation/pages/EditMyInfoPage/index.tsx
@@ -1,64 +1,184 @@
-import React, { useCallback } from "react";
+import React, { useState } from "react";
 import COLOR from "@styles/colors";
 import { styled } from "styled-components";
 import BackHeader from "@components/common/BackHeader";
 import Spacing from "@components/common/Spacing";
-import EditMyPageButton from "@components/EditMyInfoPage/components/EditMyPageButton";
-import useModal from "../../../application/hooks/useModal";
-import Modal from "@components/common/Modal";
-import EditNameModal from "@components/EditMyInfoPage/EditNameModal";
-import { useDropzone } from "react-dropzone";
 import { useNavigate } from "react-router-dom";
 import useEditProfileImage from "../../../application/hooks/queries/user/useEditProfileImage";
+import useGetMyInfo from "@hooks/queries/user/useGetMyInfo";
+import Text from "@components/common/Text";
+import { profileData } from "../../../application/data/profileData";
+import Icon from "@components/common/Icon";
+import Button from "@components/common/Button";
 
 const EditMyInfoPage = () => {
   const mutate = useEditProfileImage();
   const navigate = useNavigate();
-  const { isShowModal, toggleModal, closeModal } = useModal();
-  const handleClickEditName = () => {
-    toggleModal();
+  const { data: userData } = useGetMyInfo();
+  const [nickName, setNickName] = useState(userData.nickname);
+  const [isError, setIsError] = useState(false);
+
+  const [currProfileIdx, setCurrProfileIdx] = useState(
+    profileData.indexOf(userData.profileImageUrl)
+  );
+
+  const handleChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let value = e.target.value;
+
+    if (value.length > 5) {
+      value = value.substring(0, 5);
+    }
+
+    const pattern = /^[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]+$/;
+    if (value === "") {
+      setIsError(false);
+    } else if (pattern.test(value)) {
+      setIsError(false);
+    } else {
+      setIsError(true);
+    }
+
+    setNickName(value);
   };
-
-  const onDrop = useCallback(async (acceptedFiles: any) => {
-    const file = acceptedFiles[0];
-
-    const formData: any = new FormData();
-    formData.append("image", file);
-    mutate(formData);
-    navigate(-1);
-  }, []);
-
-  const { getRootProps, getInputProps } = useDropzone({
-    onDrop,
-    multiple: false,
-    accept: { "image/*": [] },
-  });
 
   return (
     <EditMyInfoPageWrapper>
-      <BackHeader text="회원 정보 관리" color="#191F28" />
-      <Spacing size={38} />
-      <ButtonWrapper>
-        <EditMyPageButton text="이름 변경" onClick={handleClickEditName} />
-        <div {...getRootProps()}>
-          <input {...getInputProps()} />
-          <EditMyPageButton text="프로필 이미지 변경" />
+      <BackHeader text="프로필 편집" color="#191F28" />
+      <Spacing size={46} />
+      <div className="profile-wrapper">
+        <img src={userData.profileImageUrl} alt="프로필 이미지" />
+      </div>
+      <Spacing size={34} />
+      <NameWrapper>
+        <div className="name">
+          <input type="text" value={nickName} onChange={handleChangeName} />
         </div>
+        <Text
+          text={`${nickName.length}/5`}
+          color={COLOR.GRAY_500}
+          fontSize={16}
+          fontWeight={500}
+          lineHeight="140%"
+        />
+      </NameWrapper>
+      <Spacing size={34} />
+      <ProfileImageWrapper>
+        {profileData.map((url, index) => (
+          <div className="profile-img" key={url}>
+            <img src={url} alt="프로필 이미지" />
+            <div>
+              {currProfileIdx === index ? (
+                <Icon icon="Check" />
+              ) : (
+                <div
+                  style={{
+                    width: "18px",
+                    height: "18px",
+                    border: `1.5px solid ${COLOR.GRAY_300}`,
+                    borderRadius: "100%",
+                    margin: "0 auto",
+                  }}
+                />
+              )}
+            </div>
+          </div>
+        ))}
+      </ProfileImageWrapper>
+      <Spacing size={40} />
+      <ButtonWrapper>
+        <Button
+          border="none"
+          background="#F2F4F6"
+          radius={10}
+          onClick={() => navigate(-1)}
+          padding="15px 0"
+        >
+          <Text
+            text="취소"
+            color="#505967"
+            fontSize={17}
+            fontWeight={600}
+            lineHeight="normal"
+          />
+        </Button>
+        <Button
+          border="none"
+          background={COLOR.MAIN_GREEN}
+          radius={10}
+          onClick={() => {}}
+          padding="15px 0"
+        >
+          <Text
+            text="변경하기"
+            color={COLOR.WHITE}
+            fontSize={17}
+            fontWeight={600}
+            lineHeight="normal"
+          />
+        </Button>
       </ButtonWrapper>
-      <Modal isVisible={isShowModal} closeModal={closeModal}>
-        <EditNameModal closeModal={closeModal} />
-      </Modal>
     </EditMyInfoPageWrapper>
   );
 };
 const EditMyInfoPageWrapper = styled.div`
-  background-color: ${COLOR.GRAY_50};
+  background-color: ${COLOR.WHITE};
   height: 100%;
   padding: 0 20px;
+
+  .profile-wrapper {
+    width: 120px;
+    height: 120px;
+    margin: 0 auto;
+    border-radius: 100%;
+
+    img {
+      width: 120px;
+      height: 120px;
+      border-radius: 100%;
+      display: flex;
+      justify-content: center;
+    }
+  }
+`;
+const NameWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  border-bottom: 1px solid ${COLOR.MAIN_GREEN};
+  padding-bottom: 5px;
+
+  .name {
+    input {
+      color: ${COLOR.GRAY_900};
+      font-size: 20px;
+      font-weight: 600;
+      line-height: 140%;
+      border: none;
+      outline: none;
+    }
+  }
+`;
+const ProfileImageWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  .profile-img {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    img {
+      width: 80px;
+      height: 80px;
+      border-radius: 100%;
+    }
+  }
 `;
 const ButtonWrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  gap: 20px;
+  flex-direction: row;
+  gap: 10px;
 `;
 export default EditMyInfoPage;
