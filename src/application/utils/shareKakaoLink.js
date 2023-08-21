0024ab1560da629be93bcf66043cbe75cc92a1e2
kmingki <wl1524@naver.com>
2023-08-21 22:41:02 +0900

    feat #65 : kakao 공유, 클립보드 공유기능 추가

--- src/application/utils/shareKakaoLink.js
+++ src/application/utils/shareKakaoLink.js
@@ -2,7 +2,7 @@ export const shareKakao = (route, title) => { // url이 id값에 따라 변경�
     if (window.Kakao) {
       const kakao = window.Kakao;
       if (!kakao.isInitialized()) {
-        kakao.init( process.env.REACT_APP_SHARE_KAKAO_LINK_KEY); // 카카오에서 제공받은 javascript key를 넣어줌 -> .env파일에서 호출시킴
+        kakao.init(process.env.REACT_APP_SHARE_KAKAO_LINK_KEY); // 카카오에서 제공받은 javascript key를 넣어줌 -> .env파일에서 호출시킴
       }
   
       kakao.Link.sendDefault({
