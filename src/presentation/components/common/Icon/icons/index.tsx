9885efc744e863bb83b82a4ccf0816a8fcf40436
gabi <70315572+Limgabi@users.noreply.github.com>
2023-08-23 01:34:46 +0900

    style #71 : 리스트 제작 디자인 수정

--- src/presentation/components/common/Icon/icons/index.tsx
+++ src/presentation/components/common/Icon/icons/index.tsx
@@ -7,6 +7,7 @@ export { default as Arrow } from "./Arrow";
 export { default as Bus } from "./Bus";
 export { default as Plane } from "./Plane";
 export { default as Check } from "./Check";
+export { default as UnCheck } from "./UnCheck";
 export { default as InputChecked } from "./InputChecked";
 export { default as Checked } from "./Checked";
 export { default as Plus } from "./Plus";
