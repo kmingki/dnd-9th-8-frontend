113c58bd4695a437a3f68027a39fc3e4dee966bd
gabi <70315572+Limgabi@users.noreply.github.com>
2023-08-24 17:30:52 +0900

    fix : 아이콘 수정

--- src/presentation/components/common/Icon/icons/NotExistCheckList.tsx
+++ src/presentation/components/common/Icon/icons/NotExistCheckList.tsx
@@ -9,37 +9,35 @@ const NotExistCheckList = () => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <rect
-        x="79"
-        y="110.217"
-        width="42"
-        height="20"
-        rx="10"
-        transform="rotate(-180 79 110.217)"
-        fill="url(#paint0_linear_1859_21471)"
+      <path
+        d="M79 100C79 105.523 74.5228 110 69 110L47 110C41.4772 110 37 105.523 37 100C37 94.4772 41.4772 90 47 90L69 90C74.5228 90 79 94.4772 79 100Z"
+        fill="url(#paint0_linear_1891_2182)"
       />
       <path
         fillRule="evenodd"
         clipRule="evenodd"
-        d="M75.0839 42.1329C72.5291 39.5781 68.3868 39.5781 65.832 42.1329L58.4996 49.4653L51.168 42.1337C48.6132 39.5789 44.471 39.5789 41.9161 42.1337C39.3613 44.6886 39.3613 48.8308 41.9161 51.3856L49.2477 58.7172L41.9167 66.0483C39.3618 68.6031 39.3618 72.7453 41.9167 75.3001C44.4715 77.855 48.6137 77.855 51.1685 75.3001L58.4996 67.9691L65.8315 75.3009C68.3863 77.8558 72.5285 77.8558 75.0834 75.3009C77.6382 72.7461 77.6382 68.6039 75.0833 66.0491L67.7515 58.7172L75.0839 51.3848C77.6387 48.83 77.6387 44.6878 75.0839 42.1329Z"
-        fill="url(#paint1_linear_1859_21471)"
+        d="M74.0839 41.9161C71.5291 39.3613 67.3868 39.3613 64.832 41.9161L57.4996 49.2485L50.168 41.9169C47.6132 39.3621 43.471 39.3621 40.9161 41.9169C38.3613 44.4718 38.3613 48.614 40.9161 51.1688L48.2477 58.5004L40.9167 65.8315C38.3618 68.3863 38.3618 72.5285 40.9167 75.0834C43.4715 77.6382 47.6137 77.6382 50.1685 75.0833L57.4996 67.7523L64.8315 75.0841C67.3863 77.639 71.5285 77.639 74.0834 75.0841C76.6382 72.5293 76.6382 68.3871 74.0833 65.8323L66.7515 58.5004L74.0839 51.168C76.6387 48.6132 76.6387 44.471 74.0839 41.9161Z"
+        fill="url(#paint1_linear_1891_2182)"
       />
       <path
         fillRule="evenodd"
         clipRule="evenodd"
-        d="M30 20.2168C24.4772 20.2168 20 24.694 20 30.2168V113.217C20 118.74 24.4772 123.217 30 123.217H85C90.5229 123.217 95 118.74 95 113.217V44.7168H115V113.217C115 129.785 101.569 143.217 85 143.217H30C13.4315 143.217 0 129.785 0 113.217V30.2168C0 13.6483 13.4315 0.216797 30 0.216797H75.5V20.2168H30Z"
-        fill="url(#paint2_linear_1859_21471)"
+        d="M30 20C24.4772 20 20 24.4772 20 30V113C20 118.523 24.4772 123 30 123H85C90.5229 123 95 118.523 95 113V44.5H115V113C115 129.569 101.569 143 85 143H30C13.4315 143 0 129.569 0 113V30C0 13.4315 13.4315 0 30 0H75.5V20H30Z"
+        fill="url(#paint2_linear_1891_2182)"
       />
-      <g filter="url(#filter0_d_1859_21471)">
-        <circle cx="105" cy="45.2168" r="10" fill="url(#paint3_linear_1859_21471)" />
+      <g filter="url(#filter0_d_1891_2182)">
+        <path
+          d="M115 45C115 50.5228 110.523 55 105 55C99.4772 55 95 50.5228 95 45C95 39.4772 99.4772 35 105 35C110.523 35 115 39.4772 115 45Z"
+          fill="url(#paint3_linear_1891_2182)"
+        />
       </g>
       <defs>
         <filter
-          id="filter0_d_1859_21471"
-          x="63"
-          y="3.2168"
-          width="84"
-          height="84"
+          id="filter0_d_1891_2182"
+          x="65"
+          y="5"
+          width="80"
+          height="80"
           filterUnits="userSpaceOnUse"
           colorInterpolationFilters="sRGB"
         >
@@ -49,12 +47,6 @@ const NotExistCheckList = () => {
             type="matrix"
             values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
             result="hardAlpha"
-          />
-          <feMorphology
-            radius="2"
-            operator="dilate"
-            in="SourceAlpha"
-            result="effect1_dropShadow_1859_21471"
           />
           <feOffset />
           <feGaussianBlur stdDeviation="15" />
@@ -66,58 +58,58 @@ const NotExistCheckList = () => {
           <feBlend
             mode="normal"
             in2="BackgroundImageFix"
-            result="effect1_dropShadow_1859_21471"
+            result="effect1_dropShadow_1891_2182"
           />
           <feBlend
             mode="normal"
             in="SourceGraphic"
-            in2="effect1_dropShadow_1859_21471"
+            in2="effect1_dropShadow_1891_2182"
             result="shape"
           />
         </filter>
         <linearGradient
-          id="paint0_linear_1859_21471"
-          x1="97.9138"
-          y1="120.268"
-          x2="91.3242"
-          y2="108.638"
+          id="paint0_linear_1891_2182"
+          x1="35.5"
+          y1="107"
+          x2="79.701"
+          y2="125.77"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stopColor="#A2F5D2" />
-          <stop offset="1" stopColor="#3DF3B1" stopOpacity="0.29" />
+          <stop stopColor="#EAEFF5" />
+          <stop offset="1" stopColor="#E2E7EC" />
         </linearGradient>
         <linearGradient
-          id="paint1_linear_1859_21471"
-          x1="46.9177"
-          y1="35.3744"
-          x2="69.6079"
-          y2="75.3587"
+          id="paint1_linear_1891_2182"
+          x1="45.9177"
+          y1="35.1576"
+          x2="68.6079"
+          y2="75.1419"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stopColor="#34D994" />
-          <stop offset="1" stopColor="#C9FFEB" />
+          <stop stopColor="#E9EFF4" />
+          <stop offset="1" stopColor="#E1E6EB" />
         </linearGradient>
         <linearGradient
-          id="paint2_linear_1859_21471"
+          id="paint2_linear_1891_2182"
           x1="-7.5"
-          y1="135.717"
+          y1="135.5"
           x2="81.0698"
-          y2="-11.1431"
+          y2="-11.3599"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stopColor="#34D994" />
-          <stop offset="1" stopColor="#C9FFEB" />
+          <stop stopColor="#DFE4E9" />
+          <stop offset="1" stopColor="#F8FAFF" />
         </linearGradient>
         <linearGradient
-          id="paint3_linear_1859_21471"
+          id="paint3_linear_1891_2182"
           x1="105"
-          y1="35.2168"
+          y1="35"
           x2="105"
-          y2="55.2168"
+          y2="55"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stopColor="#35D59B" />
-          <stop offset="1" stopColor="#00E6AF" />
+          <stop stopColor="#E9EEF3" />
+          <stop offset="1" stopColor="#E1E6EB" />
         </linearGradient>
       </defs>
     </svg>
