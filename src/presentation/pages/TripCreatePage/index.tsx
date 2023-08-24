38cd6391002c445a2d24894647ba4576ac26e047
gabi <70315572+Limgabi@users.noreply.github.com>
2023-08-24 21:29:13 +0900

    fix #80 : 리스트 생성 back header 추가

--- src/presentation/pages/TripCreatePage/index.tsx
+++ src/presentation/pages/TripCreatePage/index.tsx
@@ -3,7 +3,6 @@ import { useParams } from "react-router-dom";
 import { styled } from "styled-components";
 import BackHeader from "../../components/common/BackHeader";
 import PageIndicator from "../../components/common/PageIndicator";
-import Spacing from "../../components/common/Spacing";
 import Step1 from "../../components/TripCreatePage/Step1";
 import Step2 from "../../components/TripCreatePage/Step2";
 import Step3 from "../../components/TripCreatePage/Step3";
@@ -13,8 +12,7 @@ const TripCreatePage = () => {
   const pageComponents = [<Step1 />, <Step2 />, <Step3 />];
   return (
     <TripCreateTemplate>
-      {Number(step) !== 1 && <BackHeader />}
-      {Number(step) === 1 && <Spacing size={65} />}
+      <BackHeader />
       <PageIndicator />
       {pageComponents[Number(step) - 1]}
     </TripCreateTemplate>
