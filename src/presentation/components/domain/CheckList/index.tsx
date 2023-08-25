7c899f5a7ff581b7979fcd637ccf6d3178419b12
gabi <70315572+Limgabi@users.noreply.github.com>
2023-08-25 19:09:38 +0900

    style : 여행 디테일 페이지 폰트 색상 수정

--- src/presentation/components/domain/CheckList/index.tsx
+++ src/presentation/components/domain/CheckList/index.tsx
@@ -1,149 +1,212 @@
-import React, { useState,useEffect , useCallback} from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useParams } from "react-router-dom";
 import Icon from "@components/common/Icon";
-import { DragDropContext, Draggable, Droppable, DropResult } from 'react-beautiful-dnd'; //drag and drop
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  DropResult,
+} from "react-beautiful-dnd"; //drag and drop
 import Text from "@components/common/Text";
 import { CheckItem, AddCheckItem } from "./CheckItem";
-import { listItem , checkList } from "../../../../application/type/checkList";
+import { listItem, checkList } from "../../../../application/type/checkList";
 import useUpdateChecklist from "@hooks/queries/checklist/useUpdateChecklist";
 import useChangeOrderItem from "@hooks/queries/item/useChangeOrderItem";
-import { 
-    FinishButtonWrapper, 
-    FinishButton, 
-    CheckListWrapper, 
-    Head, 
-    Title, 
-    Indicator, 
-    TitleLeft, 
-    CheckItemWrapper, 
-    InputWrapper, 
-    IconWrapper, 
-    Dot,
-    DotWrapper,
-    TextWrapper,
-    TitleIconWrapper, 
-    ModalOverlay,
-    ModalWindow,
-    Content,
-    ContentWrapper,
-    ModalDeleteButton,
-    ModalSaveButton,
-    ButtonContainer,
-    Space
+import {
+  FinishButtonWrapper,
+  FinishButton,
+  CheckListWrapper,
+  Head,
+  Title,
+  Indicator,
+  TitleLeft,
+  CheckItemWrapper,
+  InputWrapper,
+  IconWrapper,
+  Dot,
+  DotWrapper,
+  TextWrapper,
+  TitleIconWrapper,
+  ModalOverlay,
+  ModalWindow,
+  Content,
+  ContentWrapper,
+  ModalDeleteButton,
+  ModalSaveButton,
+  ButtonContainer,
+  Space,
 } from "./style";
 
-type checkListType = { CheckListData: checkList; checkListId: number; order: number; title: string; itemDtoList: listItem[], onClickDeleteCheckList: any, onChangeCheckItem: any, onClickPlusItem: any, onChangeCheckItemTitle?: any, onClickDeleteCheckItem: any};
+type checkListType = {
+  CheckListData: checkList;
+  checkListId: number;
+  order: number;
+  title: string;
+  itemDtoList: listItem[];
+  onClickDeleteCheckList: any;
+  onChangeCheckItem: any;
+  onClickPlusItem: any;
+  onChangeCheckItemTitle?: any;
+  onClickDeleteCheckItem: any;
+};
 
+const AddCheckList = ({
+  CheckListData,
+  checkListId,
+  order,
+  title,
+  itemDtoList,
+  onClickDeleteCheckList,
+  onChangeCheckItem,
+  onClickPlusItem,
+  onChangeCheckItemTitle,
+  onClickDeleteCheckItem,
+}: checkListType) => {
+  const { tripId } = useParams();
+  const [checklisttitle, setTitle] = useState(title);
+  const [itemlist, setItemlist] = useState<listItem[]>(itemDtoList);
+  const [isOpen, setIsOpen] = useState(false);
+  const { mutate: updateChecklistMutate /*data , isLoading, error*/ } =
+    useUpdateChecklist();
+  const { mutate: changeOrderItemMutate /*data , isLoading, error*/ } =
+    useChangeOrderItem();
 
-const AddCheckList = ({CheckListData, checkListId, order, title, itemDtoList, onClickDeleteCheckList,onChangeCheckItem, onClickPlusItem, onChangeCheckItemTitle, onClickDeleteCheckItem}: checkListType) => {
-    const { tripId } = useParams();
-    const [ checklisttitle, setTitle] = useState(title);
-    const [ itemlist, setItemlist ] = useState<listItem[]>(itemDtoList);
-    const [isOpen, setIsOpen] = useState(false);
-    const { mutate: updateChecklistMutate /*data , isLoading, error*/ } = useUpdateChecklist();
-    const { mutate: changeOrderItemMutate /*data , isLoading, error*/ } = useChangeOrderItem();
+  useEffect(() => {
+    setItemlist(itemDtoList);
+  }, [itemDtoList]);
 
-    useEffect(()=>{
-        setItemlist(itemDtoList);
-    }, [itemDtoList])
+  const onClickOpenCheckList = () => {
+    setIsOpen((prev) => !prev);
+  };
 
-    const onClickOpenCheckList = () => {
-        setIsOpen(prev=>!prev);
-    };
+  //checklist title 수정
+  const onClickAdd = () => {
+    updateChecklistMutate({
+      travelId: Number(tripId),
+      checklistId: checkListId,
+      title: checklisttitle,
+    }); // travelId 수정 필요
+  };
 
-    //checklist title 수정
-    const onClickAdd = () => {
-        updateChecklistMutate({ travelId: Number(tripId), checklistId : checkListId, title:checklisttitle  }); // travelId 수정 필요
-    };
+  // --- Draggable이 Droppable로 드래그 되었을 때 실행되는 이벤트
+  const onDragEnd = ({ source, destination, draggableId }: DropResult) => {
+    const newItems = JSON.parse(JSON.stringify(itemlist));
+    const [removed] = newItems.splice(source.index, 1);
+    newItems.splice(Number(destination?.index), 0, removed);
 
-    // --- Draggable이 Droppable로 드래그 되었을 때 실행되는 이벤트
-    const onDragEnd = ({ source, destination, draggableId }: DropResult ) => {
+    const newItemsTmp = newItems.map((i: listItem, index: number) => {
+      return { id: i.itemId, order: index + 1 };
+    });
+    setItemlist(newItems);
 
-        const newItems = JSON.parse(JSON.stringify(itemlist));
-        const [removed] = newItems.splice(source.index, 1);
-        newItems.splice(Number(destination?.index), 0, removed);
-        console.log(newItems);
-        const newItemsTmp = newItems.map((i : listItem, index:number)=> {return { id: i.itemId, order: index+1}});
-        setItemlist(newItems);
-        
-        //console.log(newItmesTmp);
-        changeOrderItemMutate({ 
-            travelId: Number(tripId), 
-            checkListId:checkListId , 
-            data: newItemsTmp
-        }); //travel id 수정 필요
-    };
+    //console.log(newItmesTmp);
+    changeOrderItemMutate({
+      travelId: Number(tripId),
+      checkListId: checkListId,
+      data: newItemsTmp,
+    }); //travel id 수정 필요
+  };
 
-    return (
-        <>
-        <CheckListWrapper>
-            <Head>
-                <Title>
-                    {CheckListData?.essential? 
-                    <TextWrapper>
-                    <Text text={title} color="#232527" fontSize={16} lineHeight="21.12px" fontWeight={600} />
-                    <DotWrapper><Dot /></DotWrapper>
-                    </TextWrapper>
-                    :
-                    <InputWrapper placeholder={checklisttitle} onBlur={onClickAdd} onChange={(e: React.ChangeEvent<HTMLInputElement>)=>{setTitle(e.target.value)}} value={checklisttitle}/>
-                    }
-                    
-                    <TitleLeft>
-                    <Indicator>{itemDtoList.filter((item) => item.isChecked === true).length}/{itemDtoList.length}</Indicator>
-                    {isOpen? 
-                    <IconWrapper>
-                        <Icon icon='UpOutlined' onClick={onClickOpenCheckList}/>
-                    </IconWrapper>:
-                    <IconWrapper>
-                        <Icon icon='DownOutlined' onClick={onClickOpenCheckList}/>
-                    </IconWrapper>}
-                </TitleLeft>
-                </Title>
-            </Head>
+  return (
+    <>
+      <CheckListWrapper>
+        <Head>
+          <Title>
+            {CheckListData?.essential ? (
+              <TextWrapper>
+                <Text
+                  text={title}
+                  color="#232527"
+                  fontSize={16}
+                  lineHeight="21.12px"
+                  fontWeight={600}
+                />
+                <DotWrapper>
+                  <Dot />
+                </DotWrapper>
+              </TextWrapper>
+            ) : (
+              <InputWrapper
+                placeholder={checklisttitle}
+                onBlur={onClickAdd}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setTitle(e.target.value);
+                }}
+                value={checklisttitle}
+              />
+            )}
 
-            {isOpen && 
-            <CheckItemWrapper>
-                <DragDropContext onDragEnd={onDragEnd}>
-                    <Droppable droppableId="droppable">
+            <TitleLeft>
+              <Indicator>
+                <span>
+                  {itemDtoList.filter((item) => item.isChecked === true).length}
+                </span>
+                /{itemDtoList.length}
+              </Indicator>
+              {isOpen ? (
+                <IconWrapper>
+                  <Icon icon="UpOutlined" onClick={onClickOpenCheckList} />
+                </IconWrapper>
+              ) : (
+                <IconWrapper>
+                  <Icon icon="DownOutlined" onClick={onClickOpenCheckList} />
+                </IconWrapper>
+              )}
+            </TitleLeft>
+          </Title>
+        </Head>
+
+        {isOpen && (
+          <CheckItemWrapper>
+            <DragDropContext onDragEnd={onDragEnd}>
+              <Droppable droppableId="droppable">
+                {(provided: any) => (
+                  <div ref={provided.innerRef} {...provided.droppableProps}>
+                    {itemlist.map((item, index) => (
+                      <Draggable
+                        key={item.itemId}
+                        draggableId={String(item.itemId)}
+                        index={index}
+                      >
                         {(provided: any) => (
-                        <div ref={provided.innerRef} {...provided.droppableProps}>
-                            {itemlist.map((item, index) => (
-                            <Draggable key={item.itemId} draggableId={String(item.itemId)} index={index}>
-                                {(provided : any) => (
-                                <div
-                                    ref={provided.innerRef}
-                                    {...provided.draggableProps}
-                                    {...provided.dragHandleProps}
-                                >
-                                    <CheckItem 
-                                    checkListId={checkListId} 
-                                    itemId={item.itemId} 
-                                    isChecked={item.isChecked} 
-                                    title={item.title} 
-                                    onChangeCheckItem={onChangeCheckItem} 
-                                    onChangeCheckItemTitle={onChangeCheckItemTitle}
-                                    onClickDeleteCheckItem={onClickDeleteCheckItem}
-                                    />
-                                </div>
-                                )}
-                            </Draggable>
-                            ))}
-                            {provided.placeholder}
-                        </div>
+                          <div
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                          >
+                            <CheckItem
+                              checkListId={checkListId}
+                              itemId={item.itemId}
+                              isChecked={item.isChecked}
+                              title={item.title}
+                              onChangeCheckItem={onChangeCheckItem}
+                              onChangeCheckItemTitle={onChangeCheckItemTitle}
+                              onClickDeleteCheckItem={onClickDeleteCheckItem}
+                            />
+                          </div>
                         )}
-                    </Droppable>
-                </DragDropContext>
-                <AddCheckItem checkListId={checkListId} id={itemDtoList.length} onClickPlusItem={onClickPlusItem}/>
-                <FinishButtonWrapper>
-                    <FinishButton onClick={()=>onClickDeleteCheckList(checkListId)}>
-                        삭제하기
-                    </FinishButton>
-                </FinishButtonWrapper>
-            </CheckItemWrapper>
-            }
-        </CheckListWrapper>
-        </>
-    );
-}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </DragDropContext>
+            <AddCheckItem
+              checkListId={checkListId}
+              id={itemDtoList.length}
+              onClickPlusItem={onClickPlusItem}
+            />
+            <FinishButtonWrapper>
+              <FinishButton onClick={() => onClickDeleteCheckList(checkListId)}>
+                삭제하기
+              </FinishButton>
+            </FinishButtonWrapper>
+          </CheckItemWrapper>
+        )}
+      </CheckListWrapper>
+    </>
+  );
+};
 
 export { AddCheckList };
--- src/presentation/components/domain/CheckList/style.ts
+++ src/presentation/components/domain/CheckList/style.ts
@@ -1,230 +1,227 @@
 import { styled } from "styled-components";
-import Color from '@styles/colors';
+import Color from "@styles/colors";
 import COLOR from "@styles/colors";
 const Dot = styled.div`
-    border-radius : 50%;
-    background-color: ${COLOR.MAIN_GREEN};
-    height: 5px;
-    width: 5px;
-`
+  border-radius: 50%;
+  background-color: ${COLOR.MAIN_GREEN};
+  height: 5px;
+  width: 5px;
+`;
 const DotWrapper = styled.div`
-    margin-left : 2px;
-    display : flex;
-    flex-direction: column;
-    justify-content: flex-start;
+  margin-left: 2px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
 `;
 
 const TextWrapper = styled.div`
-    display : flex;
+  display: flex;
 `;
 const FinishButtonWrapper = styled.div`
-    display : flex;
-    justify-content: flex-end;
-    margin-top : 10px;
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 10px;
 `;
 
 const FinishButton = styled.button`
-    all : unset;
-    border : 0px;
-    font-weight: 500;
-    font-size : 13px;
-    line-height: 15px;
-    text-decoration : underline;
-    color : ${Color.GRAY_700};
+  all: unset;
+  border: 0px;
+  font-weight: 500;
+  font-size: 13px;
+  line-height: 15px;
+  text-decoration: underline;
+  color: ${Color.GRAY_700};
 `;
 
 const CheckListWrapper = styled.div`
-    padding: 19px 17px;
-    margin-bottom: 20px;
-    background-color: ${Color.WHITE};
-    border-radius: 12px;
-    border : 2px solid #E9ECF0;
+  padding: 19px 17px;
+  margin-bottom: 20px;
+  background-color: ${Color.WHITE};
+  border-radius: 12px;
+  border: 2px solid #e9ecf0;
 `;
 
 const Head = styled.div`
-    display: flex;
-    justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 `;
-
 
 const Indicator = styled.div`
-    width: 41px;
-    height: 24px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: ${Color.WHITE};
-    border-radius: 38px;
-    margin-right : 9px;
-    border: 1.5px solid #E9ECF0;
+  width: 41px;
+  height: 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${Color.WHITE};
+  border-radius: 38px;
+  margin-right: 9px;
+  border: 1.5px solid #e9ecf0;
 
-    color: ${COLOR.GRAY_700};
-    font-weight: 500;
-    font-size: 12px;
-    line-height: 16px;
+  color: ${COLOR.GRAY_700};
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 16px;
+
+  span {
+    color: ${COLOR.MAIN_GREEN};
+  }
 `;
-
 
 const Title = styled.div`
-    display: flex;
-    justify-content: space-between;
-    width : 100%;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
 `;
 
-
 const TitleLeft = styled.div`
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 `;
 
 const TitleRight = styled.div`
-    display: flex;
+  display: flex;
 `;
 
 const CheckItemWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin-top: 18px;
-`
-
-const InputWrapper = styled.input`
-    width:100%;
-    border: none;
-    background: transparent;
-    &:focus {
-        outline:none;
-    }
-    font-weight :600;
-    font-size: 16px;
-    line-height: 21.12px;
-    color: ${COLOR.GRAY_800};
+  display: flex;
+  flex-direction: column;
+  margin-top: 18px;
 `;
 
+const InputWrapper = styled.input`
+  width: 100%;
+  border: none;
+  background: transparent;
+  &:focus {
+    outline: none;
+  }
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 21.12px;
+  color: ${COLOR.GRAY_800};
+`;
 
 const IconWrapper = styled.button`
-    all : unset;
-    border : 0px;
-    padding-right: 8px;
+  all: unset;
+  border: 0px;
+  padding-right: 8px;
 `;
 
 const TitleIconWrapper = styled.button`
-    all : unset;
-    border : 0px;
-    padding-right: 8px;
+  all: unset;
+  border: 0px;
+  padding-right: 8px;
 `;
 
 const ModalOverlay = styled.div`
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    left: 0;
-    top: 0;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: flex-end;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
 `;
 
 const ModalWindow = styled.div`
-    background: ${Color.WHITE};
-    box-shadow: 0px 0px 10px 0px #85858540;
-    backdrop-filter: blur( 13.5px );
-    -webkit-backdrop-filter: blur( 13.5px );
-    border-radius: 16px;
-    height: 197px;
-    width : 80%;
-    max-width : 420px;
-    position: relative;
-    margin : 0 0 47px 0 ;
+  background: ${Color.WHITE};
+  box-shadow: 0px 0px 10px 0px #85858540;
+  backdrop-filter: blur(13.5px);
+  -webkit-backdrop-filter: blur(13.5px);
+  border-radius: 16px;
+  height: 197px;
+  width: 80%;
+  max-width: 420px;
+  position: relative;
+  margin: 0 0 47px 0;
 `;
-
 
 const Content = styled.div`
-    padding : 20px 20px 0px 20px;
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-    justify-items: center;
-    align-content: center;
+  padding: 20px 20px 0px 20px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  justify-items: center;
+  align-content: center;
 `;
 
-
-const ContentWrapper = styled.button<{ id : string, titleEmoji : string}>`
-    all : unset;
-    border : 0px;
-    width: 40px;
-    height: 40px;
-    border-radius: 50%;
-    background-color: ${Color.GRAY_50};
-    display: flex;
-    justify-content: center;
-    margin-bottom: 16px;
-    z-index : 50;
-    ${({id, titleEmoji})=>id === titleEmoji? `border : 2px solid ${Color.MAIN_GREEN}`:``};
-
+const ContentWrapper = styled.button<{ id: string; titleEmoji: string }>`
+  all: unset;
+  border: 0px;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: ${Color.GRAY_50};
+  display: flex;
+  justify-content: center;
+  margin-bottom: 16px;
+  z-index: 50;
+  ${({ id, titleEmoji }) =>
+    id === titleEmoji ? `border : 2px solid ${Color.MAIN_GREEN}` : ``};
 `;
 
 const ModalDeleteButton = styled.button`
-    all : unset;
-    border : 0px;    
-    width: 142px;
-    height : 45px;
-    border-radius: 10px;
-    background-color : #F2F4F6;
-    text-align: center;
-    margin-bottom : 20px;
-    font-weight: 600;
-    font-size: 17px;
-    line-height: 20.4px;
+  all: unset;
+  border: 0px;
+  width: 142px;
+  height: 45px;
+  border-radius: 10px;
+  background-color: #f2f4f6;
+  text-align: center;
+  margin-bottom: 20px;
+  font-weight: 600;
+  font-size: 17px;
+  line-height: 20.4px;
 `;
 
-
 const ModalSaveButton = styled.button`
-    all : unset;
-    border : 0px;
-    width: 142px;
-    height : 45px;
-    border-radius: 10px;
-    background-color : ${Color.MAIN_GREEN};
-    text-align: center;
-    margin-bottom : 20px;
-    font-weight: 600;
-    font-size: 17px;
-    line-height: 20.4px;
-    color: ${Color.WHITE};
+  all: unset;
+  border: 0px;
+  width: 142px;
+  height: 45px;
+  border-radius: 10px;
+  background-color: ${Color.MAIN_GREEN};
+  text-align: center;
+  margin-bottom: 20px;
+  font-weight: 600;
+  font-size: 17px;
+  line-height: 20.4px;
+  color: ${Color.WHITE};
 `;
 
 const ButtonContainer = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Space = styled.div`
-    width: 12px;
+  width: 12px;
 `;
 
-export { 
-    Dot,
-    DotWrapper,
-    TextWrapper,
-    FinishButtonWrapper, 
-    FinishButton, 
-    CheckListWrapper, 
-    Head, 
-    Title, 
-    Indicator, 
-    TitleLeft, 
-    TitleRight,
-    CheckItemWrapper, 
-    InputWrapper, 
-    IconWrapper, 
-    TitleIconWrapper, 
-    ModalOverlay,
-    ModalWindow,
-    Content,
-    ContentWrapper,
-    ModalDeleteButton,
-    ModalSaveButton,
-    ButtonContainer,
-    Space
+export {
+  Dot,
+  DotWrapper,
+  TextWrapper,
+  FinishButtonWrapper,
+  FinishButton,
+  CheckListWrapper,
+  Head,
+  Title,
+  Indicator,
+  TitleLeft,
+  TitleRight,
+  CheckItemWrapper,
+  InputWrapper,
+  IconWrapper,
+  TitleIconWrapper,
+  ModalOverlay,
+  ModalWindow,
+  Content,
+  ContentWrapper,
+  ModalDeleteButton,
+  ModalSaveButton,
+  ButtonContainer,
+  Space,
 };
